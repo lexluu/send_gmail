fa3ab1ec0cf51a94be99390e2cfd2c334daf5d6e
lexluu <48651287+lexluu@users.noreply.github.com>
2022-04-26 14:27:48 -0500

    Update send_gmail.py

--- send_gmail.py
+++ send_gmail.py
@@ -1,7 +1,8 @@
 # Author: Alex Luu
 # Date: 20 April 2022
+# Last Updated: 26 April 2022
 # Purpose: Logging in and sending an email through Selenium and Python
-# In progress: Creating an email account, stalled by TFA
+# In progress: Classes and function overloading
 
 
 from selenium import webdriver
@@ -28,6 +29,8 @@ password = "S0meP@ssword"
 to_email = email
 subject = 'example subject'
 body = 'example body'
+list_of_fonts = ['Sans Serif', 'Serif', 'Fixed Width', 'Wide', 'Narrow', 'Comic Sans MS',
+                 'Garamond', 'Georgia', 'Tahoma', 'Trebuchet MS', 'Verdana']
 
 
 # Clicks the "next" button for gmail, which is used multiple times
@@ -38,50 +41,76 @@ def gmail_next():
     next_button.click()
 
 
-# Creates a gmail account. Function is stalled at the moment due to TFA.
-
-def create_gmail(create_first_name, create_last_name, create_username, create_password):
-    get_gmail = driver.find_element(By.XPATH, "//span[contains(text(), 'Get Gmail')]")
-    get_gmail.click()
-    first_name_field = driver.find_element(By.ID, "firstName")
-    first_name_field.send_keys(create_first_name)
-    last_name_field = driver.find_element(By.ID, "lastName")
-    last_name_field.send_keys(create_last_name)
-    username_field = last_name_field = driver.find_element(By.ID, "username")
-    username_field.send_keys(create_username)
-    password_field = driver.find_element(By.NAME, "Passwd")
-    password_field.send_keys(create_password)
-    confirm_password_field = driver.find_element(By.NAME, "ConfirmPasswd")
-    confirm_password_field.send_keys(create_password)
-    gmail_next()
-    # next steps involve a phone, so this is going to be manual for now
-    # therefore this function is incomplete
-
-
 # Logging into gmail with an existing account that's been used on this computer before
 
 def log_into_gmail(login_email, login_password):
-    driver.get('https://www.google.com/gmail/about/')
-    sign_in_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Sign in')]")
-    sign_in_button.click()
-    input_email = driver.find_element(By.ID, "identifierId")
-    input_email.send_keys(login_email)
-    gmail_next()
-    input_password = driver.find_element(By.CSS_SELECTOR, "[aria-label*='Enter your password']")
-    input_password.send_keys(login_password)
-    gmail_next()
+    try:
+        driver.get('https://www.google.com/gmail/about/')
+        sign_in_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Sign in')]")
+        sign_in_button.click()
+        input_email = driver.find_element(By.ID, "identifierId")
+        input_email.send_keys(login_email)
+        gmail_next()
+        input_password = driver.find_element(By.CSS_SELECTOR, "[aria-label*='Enter your password']")
+        input_password.send_keys(login_password)
+        gmail_next()
+    except Exception:
+        # sometimes it needs to retry
+        log_into_gmail(login_email, login_password)
 
 
-# send an email with basic text
+# Change font
+
+def change_font(font_number_from_list):
+    try:
+        formatting_button = wait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@data-tooltip, 'Formatting options')]")))
+        formatting_button.click()
+        default_font = wait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@aria-label, '(Ctrl-Shift-5, Ctrl-Shift-6)')]")))
+        default_font.click()
+        new_font = wait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), '" +
+                                        list_of_fonts[font_number_from_list] + "')]")))
+        new_font.click()
+    except Exception:
+        # sometimes needs to be done twice
+        change_font(font_number_from_list)
+
+
+# send an email with choice in font
+
+def send_email_new_font(target_email, email_subject, body_content, font_number):
+    compose_button = driver.find_element(By.XPATH, "//div[contains(text(), 'Compose')]")
+    compose_button.click()
+    to_field_enter = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "to")))
+    to_field_enter.send_keys(target_email)
+    subject_title = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "subjectbox")))
+    subject_title.send_keys(email_subject)
+    change_font(font_number)
+    email_body = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "[role*='textbox']")))
+    email_body.send_keys(body_content)
+    send_button = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@aria-label, '(Ctrl-Enter)')]")))
+    send_button.click()
+
+
+#  for default font only
 
 def send_email(target_email, email_subject, body_content):
     compose_button = driver.find_element(By.XPATH, "//div[contains(text(), 'Compose')]")
     compose_button.click()
-    to_field_enter = driver.find_element(By.NAME, "to")
+    to_field_enter = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "to")))
     to_field_enter.send_keys(target_email)
-    subject_title = driver.find_element(By.NAME, "subjectbox")
+    subject_title = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "subjectbox")))
     subject_title.send_keys(email_subject)
-    email_body = driver.find_element(By.CSS_SELECTOR, "[role*='textbox']")
+    email_body = wait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "[role*='textbox']")))
     email_body.send_keys(body_content)
     send_button = wait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//div[contains(@aria-label, '(Ctrl-Enter)')]")))
@@ -89,6 +118,6 @@ def send_email(target_email, email_subject, body_content):
 
 
 def test():
-    # create_gmail(first_name, last_name, username, password)
     log_into_gmail(email, password)
-    send_email(to_email, subject, body)
+    send_email_new_font(to_email, subject, body, 2)
+    driver.close()
